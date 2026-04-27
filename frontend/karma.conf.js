308c01a999e6a1bad08059270a7029d9e491c2c0
José Ortiz Hernández <160781845+JAntonio2005@users.noreply.github.com>
2026-04-27 13:42:43 -0600

    Pull request sobre develop a master para hacer el workflow de Sonar-02 (#8)
    * feat: agregar spec 006 nginx proxy y pipeline CI
    
    * Subir cambios de cypress y también el action
    
    * Compuse el job de test_frontend
    
    * fix(frontend): sync lockfile for npm 10 CI
    
    * ci: agregar workflow Sonar-02 para SonarCloud
    
    * ci: fix Sonar-02 Chrome sandbox issue in Karma tests
    
    * ci: actualizar Sonar-02 a v6 y configurar java binaries
    
    * chore(frontend): resolver code smells de Sonar en karma config

---
+++ frontend/karma.conf.js
@@ -0,0 +1,46 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+const path = require('node:path');
+
+module.exports = function configureKarma(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+    ],
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: path.join(__dirname, './coverage/frontend'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcov' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+      }
+    },
+    restartOnFileChange: true
+  });
+};
